5bb0f8ef391f4d8bfb7ea91d35687b451315463a
angel umwiza <angelumwiza21@gmail.com>
2023-06-29 08:48:41 +0300

    made more additions

---
+++ loops.js
@@ -0,0 +1,21 @@
+//1.for loops
+//2.while loops
+//3.do while loops
+
+// 1.For loops
+//for(initialisation; condition; increment){
+//    body
+//}
+let studentNumber;
+for(studentNumber = 0; studentNumber <= 10; studentNumber+=2){
+     console.log("value of studentnumber is"  +" "  +  studentNumber)
+}
+let oddNumbers;
+for(oddNumber = 1; oddNumber <= 10; oddNumber+=2){
+    console.log("oddnumbers"  +" "  +  oddNumber)
+}
+//create 5 if statements
+//create 5 switch cases
+//create a switch case for arrays
+
+
